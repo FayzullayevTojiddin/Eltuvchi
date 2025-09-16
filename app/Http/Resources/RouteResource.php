eda950fa65c3cf67994d6d7bf955d8608fe6cd2e
FayzullaevTojiddin <tfayzullaev66@gmail.com>
2025-09-16 20:12:20 +0500

    Route Resource Fixed

--- app/Http/Resources/RouteResource.php
+++ app/Http/Resources/RouteResource.php
@@ -35,8 +35,10 @@ class RouteResource extends JsonResource
     {
         return [
             'id' => $this->id,
-            'from' => new TaxoParkResource($this->whenLoaded('fromTaxopark')),
-            'to' => new TaxoParkResource($this->whenLoaded('toTaxopark')),
+//            'from' => new TaxoParkResource($this->whenLoaded('fromTaxopark')),
+//            'to' => new TaxoParkResource($this->whenLoaded('toTaxopark')),
+            'from' => new TaxoParkResource($this->fromTaxopark),
+            'to' => new TaxoParkResource($this->toTaxopark),
             'status' => $this->status,
             'deposit_client' => $this->deposit_client,
             'distance_km' => $this->distance_km,
