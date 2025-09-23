f8fd6559bf4644b983dbf78ddc88205792c8b7d1
FayzullaevTojiddin <tfayzullaev66@gmail.com>
2025-09-23 14:08:19 +0500

    Test controller ficc

--- app/Http/Controllers/TestController.php
+++ app/Http/Controllers/TestController.php
@@ -11,7 +11,7 @@ class TestController extends Controller
         $user = Auth::user();
         return [
             'user' => $user,
-            // 'details' => $user->connected
+            'details' => $user->connected
         ];
     }
 }
