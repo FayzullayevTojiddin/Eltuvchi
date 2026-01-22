b2a2221fb42c96fbf59dc7a6076c02d21bf8a1a8
FayzullayevTojiddin <tfayzullaev66@gmail.com>
2026-01-22 12:20:18 +0500

    bug fix

--- app/Http/Controllers/Telegram/BalanceHandler.php
+++ app/Http/Controllers/Telegram/BalanceHandler.php
@@ -11,7 +11,7 @@ class BalanceHandler extends BaseTelegramController
             return;
         }
 
-        $connected = $user->connected;
+        $connected = $user->connected();
         
         if (!$connected) {
             $this->sendMessage($chatId, "❌ Hisob ma'lumotlari topilmadi.", $this->getMainKeyboard($user));
