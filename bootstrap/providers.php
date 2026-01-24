06d8d70cdda8701653e1a1dca74b8db77edce4e2
Azamat-akoooo <tojiddinfayzullaev981@gmail.com>
2026-01-24 11:39:15 +0500

    ef

--- bootstrap/providers.php
+++ bootstrap/providers.php
@@ -3,7 +3,6 @@
 return [
     App\Providers\AppServiceProvider::class,
     App\Providers\BroadcastServiceProvider::class,
-    App\Providers\EventServiceProvider::class,
     App\Providers\Filament\SuperAdminPanelProvider::class,
     App\Providers\Filament\TaxoParkAdminPanelProvider::class,
     App\Providers\TelescopeServiceProvider::class,
