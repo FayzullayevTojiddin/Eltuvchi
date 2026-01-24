5b66ddacc648a05138d1604c8734910ffb61fa60
Azamat-akoooo <tojiddinfayzullaev981@gmail.com>
2026-01-24 11:35:35 +0500

    removed

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,9 +2,7 @@
 
 namespace App\Providers;
 
-use App\Models\Client;
 use App\Models\Order;
-use App\Observers\ClientObserve;
 use App\Observers\OrderObserver;
 use Illuminate\Support\ServiceProvider;
 
