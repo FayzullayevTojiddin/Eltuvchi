58c1dbac5d802912cba48d5b99c6deed05daf932
Azamat-akoooo <tojiddinfayzullaev981@gmail.com>
2026-01-25 05:24:44 +0500

    edited

--- app/Http/Resources/DriverProductResource.php
+++ app/Http/Resources/DriverProductResource.php
@@ -4,6 +4,7 @@ namespace App\Http\Resources;
 
 use Illuminate\Http\Request;
 use Illuminate\Http\Resources\Json\JsonResource;
+use Illuminate\Support\Facades\Storage;
 
 class DriverProductResource extends JsonResource
 {
@@ -12,7 +13,7 @@ class DriverProductResource extends JsonResource
         return [
             'id'          => $this->id,
             'status'      => (bool) $this->status,
-            'icon_type'   => $this->product->icon_type,
+            'icon_type'   => Storage::url($this->product->icon_type),
             'points'      => $this->product->points,
             'title'       => $this->product->title,
             'description' => $this->product->description,
